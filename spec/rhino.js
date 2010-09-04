4cc58ea2df5bd400b60843cb61bbcaa35919628f
Ronald Kaiser <raios.catodicos@gmail.com>
2010-09-04 15:45:22 -0300

    adding jspec template files

---
+++ spec/rhino.js
@@ -0,0 +1,10 @@
+
+load('/opt/visionmedia-jspec-df48355/lib/jspec.js')
+load('/opt/visionmedia-jspec-df48355/lib/jspec.xhr.js')
+load('lib/yourlib.js')
+load('spec/unit/spec.helper.js')
+
+JSpec
+.exec('spec/unit/spec.js')
+.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+.report()
